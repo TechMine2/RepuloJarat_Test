4f3c5f48ad1f2a83109f29553721728eb19242ee
Csaba <nycs007@gmail.com>
2019-03-02 15:27:36 +0100

    Órai munka importálása

--- RepuloJarat/Class1.cs
+++ RepuloJarat/Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,143 @@ using System.Threading.Tasks;
 
 namespace RepuloJarat
 {
-    public class Class1
+    public class JaratKezelo
     {
+        List<Jarat> jaratok = new List<Jarat>();
+
+        public void UjJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+        {
+            foreach (var item in this.jaratok)
+            {
+                if (item.Jaratszam == jaratSzam)
+                {
+                    ArgumentException e = new ArgumentException("Egyező járat" + jaratSzam);
+                    throw e;
+                }
+            }
+            Jarat jarat = new Jarat();
+            jarat.Jaratszam = jaratSzam;
+            jarat.RepterHonnan = repterHonnan;
+            jarat.RepterHova = repterHova;
+            jarat.Indulas = indulas;
+            jarat.Keses = 0;
+
+
+            jaratok.Add(jarat);
+        }
+
+        public void Keses(string jaratSzam, int keses)
+        {
+            foreach (var item in jaratok)
+            {
+                if (item.Jaratszam == jaratSzam)
+                {
+                    item.Keses += keses;
+                }
+            }
+        }
+
+        public DateTime MikorIndul(string jaratSzam)
+        {
+            foreach (var item in jaratok)
+            {
+                if (item.Jaratszam == jaratSzam)
+                {
+                    return item.Indulas;
+                }
+            }
+            throw new ArgumentException();
+        }
+
+        public List<string> JaratokRepuloterrol(string repter)
+        {
+            List<string> jaratok = new List<string>();
+            return jaratok;
+        }
+
+        public int Jaratkesese(string jaratszam)
+        {
+            foreach (var item in jaratok)
+            {
+                return item.Keses;
+            }
+            throw new ArgumentException();
+        }
     }
+
+    public class Jarat
+    {
+        private string jaratszam;
+        private string repterHonnan;
+        private string repterHova;
+        private DateTime indulas;
+        private int keses;
+
+        public string Jaratszam
+        {
+            get
+            {
+                return jaratszam;
+            }
+
+            set
+            {
+                jaratszam = value;
+            }
+        }
+
+        public string RepterHonnan
+        {
+            get
+            {
+                return repterHonnan;
+            }
+
+            set
+            {
+                repterHonnan = value;
+            }
+        }
+
+        public string RepterHova
+        {
+            get
+            {
+                return repterHova;
+            }
+
+            set
+            {
+                repterHova = value;
+            }
+        }
+
+        public DateTime Indulas
+        {
+            get
+            {
+                return indulas;
+            }
+
+            set
+            {
+                indulas = value;
+            }
+        }
+
+        public int Keses
+        {
+            get
+            {
+                return keses;
+            }
+
+            set
+            {
+                keses = value;
+            }
+        }
+
+    }
+
 }
---
+++ RepuloJarat/JaratKezeloTeszt.cs
@@ -0,0 +1,53 @@
+﻿﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepuloJarat
+{
+    [TestFixture]
+    class JaratKezeloTeszt
+    {
+        [Test]
+        public void UjJaratJokorIndul()
+        {
+            JaratKezelo jk = new JaratKezelo();
+            jk.UjJarat("S213", "Budapest", "Berlin", new DateTime(2019, 03, 01, 12, 50, 0));
+            Assert.AreEqual(
+                new DateTime(2019, 03, 01, 12, 50, 0),
+                jk.MikorIndul("S213")
+                );
+        }
+        [Test]
+        public void UjJaratEgyezonev()
+        {
+            JaratKezelo jk = new JaratKezelo();
+            jk.UjJarat("S213", "Budapest", "Berlin", new DateTime(2019, 03, 01, 12, 50, 0));
+
+            Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    jk.UjJarat("S213", "New York", "Berlin", new DateTime(2019, 03, 01, 10, 51, 0));
+                }
+            );
+
+
+        }
+
+        [Test]
+        public void Keses()
+        {
+            JaratKezelo jk = new JaratKezelo();
+            jk.UjJarat("S213", "Budapest", "Berlin", new DateTime(2019, 03, 01, 12, 50, 0));
+            jk.Keses("S213", 5);
+
+            Assert.AreEqual(
+                5,
+                jk.Jaratkesese("S213")
+                );
+
+        }
+    }
+}
